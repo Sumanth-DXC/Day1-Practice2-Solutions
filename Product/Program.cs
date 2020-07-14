b19992697b01a60626b0f51adc8a6959df4d78be
Sumanth-DXC <1210316255@gitam.in>
2020-07-14 15:41:16 +0530

    final commit

---
+++ Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.Set(5000, "Xiaomi");
+            p1.Get();
+        }
+    }
+}
---
+++ Product/product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Product
+    {
+        private int cost;
+        private string brand;
+
+        internal void Set(int cost, string brand)
+        {
+            this.cost = cost;
+            this.brand = brand;
+        }
+
+        internal void Get()
+        {
+            Console.WriteLine($"Brand: {brand} Cost: {cost}");
+        }
+
+    }
+}
